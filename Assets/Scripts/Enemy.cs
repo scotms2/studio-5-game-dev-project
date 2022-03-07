3653e5718efcaa2f1f77d8640e371b61b9f24dd2
mitch-scott <mitchellscott11@hotmail.com>
2022-03-07 20:49:28 +1300

    added player tracking so the cubes will follow the player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,10 +8,26 @@ public class Enemy : MonoBehaviour
 
     public float runSpeed;
 
+    public GameObject player;
+
+    int MinDist = 5;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * runSpeed * Time.deltaTime);
+        //transform.Translate(Vector3.forward * runSpeed * Time.deltaTime);
+
+        transform.LookAt(player.transform.position);
+
+        if (Vector3.Distance(transform.position, player.transform.position) >= MinDist)
+        {
+            transform.position += transform.forward * runSpeed * Time.deltaTime;
+        }
     }
 
     public void SetSpawner(Spawner spawner)
