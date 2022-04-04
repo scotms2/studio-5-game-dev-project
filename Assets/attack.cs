9ed3dad3f5109c0c09e0856d26d948ca7bcfcd96
unknown <portfa1@student.op.ac.nz>
2022-04-04 14:08:13 +1200

    only hitting when animation activated

--- Assets/attack.cs
+++ Assets/attack.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class attack : MonoBehaviour
 {
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = transform.root.gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,7 +22,12 @@ public class attack : MonoBehaviour
     {
         if (other.gameObject.tag == "playerTrigger")
         {
-            Debug.Log("hit player");
+            
+            //animationStateController temp = GetComponent(typeof(animationStateController)) as animationStateController;
+            if(animator.GetBool("isPunch"))
+            {
+                Debug.Log("hit player");
+            }
         }
             
     }
